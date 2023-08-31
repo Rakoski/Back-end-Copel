52151ba5d187dc93562dfe2e91e0eccf74f4072d
Rakoski <mastrakoski@gmail.com>
2023-08-31 14:27:00 -0300

    Built-in java!

---
+++ src/main/java/com/example/hackathoncopel/HackathonCopelApplication.java
@@ -0,0 +1,13 @@
+package com.example.hackathoncopel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonCopelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackathonCopelApplication.class, args);
+    }
+
+}
